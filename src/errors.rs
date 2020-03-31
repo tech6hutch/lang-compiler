e9c31d42bc62e37f7befbc828d0e6ebb9399867d
tech6hutch <tech_hutch@verizon.net>
2020-03-30 23:33:53 -0400

    added display for syntax errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,10 @@
-use std::fmt::{self, Display, Debug, Formatter};
-use std::error::Error;
+use std::{
+    error::Error,
+    fmt::{self, Display, Debug, Formatter},
+};
+use unicode_names2::name;
 use crate::text::Span;
 
-#[derive(Debug)]
 pub enum SyntaxError {
     UnexpectedCharacter(Vec<char>, Span),
     UnterminatedString(Span),
@@ -11,7 +13,34 @@ pub enum SyntaxError {
 
 impl Display for SyntaxError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        todo!();
+        use SyntaxError::*;
+        f.write_str("Syntax error: ").and_then(|_| match self {
+            UnexpectedCharacter(chars, span) => {
+                f.write_str(if chars.len() == 1 {
+                    if let Some(name) = name(chars[0]) {
+                        format!("Unexpected character '{}' at {}", name, span)
+                    } else {
+                        format!("Unexpected character at {}", span)
+                    }
+                } else {
+                    format!("Unexpected characters at {}", span)
+                }.as_str())
+            },
+
+            UnterminatedString(span) => {
+                f.write_str(format!("The string at {} isn't terminated", span).as_str())
+            },
+
+            StringInvalidEscSeq(span) => {
+                f.write_str(format!("The string escape sequence at {} is invalid", span).as_str())
+            },
+        })
+    }
+}
+
+impl Debug for SyntaxError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        Display::fmt(self, f)
     }
 }
 
